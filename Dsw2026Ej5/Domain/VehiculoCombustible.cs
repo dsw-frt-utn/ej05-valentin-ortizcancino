00d9119751a43059582d5cc44a9be880075f70b0
Valentin Cancino <eduardozurita108@gmail.com>
2026-04-22 22:10:21 -0300

    Revert "Solucion error calculo consumo"
    his reverts commit 7b095d368990add0a93bcf7c0ea05af2cad134f3.

--- Dsw2026Ej5/Domain/VehiculoCombustible.cs
+++ Dsw2026Ej5/Domain/VehiculoCombustible.cs
@@ -4,17 +4,16 @@ using System.Text;
 
 namespace Dsw2026Ej5.Domain;
 
-public class VehiculoCombustible : Vehiculo
+public class VehiculoCombustible: Vehiculo
 {
     private double kilometrosPorLitro;
     private double litrosExtra;
 
-    public VehiculoCombustible(string patente, string marca, string modelo, int anio, double capacidadCarga,
+    public VehiculoCombustible(string patente, string marca, string modelo, int anio, double capacidadCarga, 
         Sucursal sucursal, double kilometrosPorLitro, double litrosExtra) : base(VehiculoTipo.Combustible, patente, marca, modelo, anio, capacidadCarga, sucursal)
     {
         this.kilometrosPorLitro = kilometrosPorLitro;
         this.litrosExtra = litrosExtra;
-
     }
 
     public double GetKilometrosPorLitro()
@@ -29,13 +28,6 @@ public class VehiculoCombustible : Vehiculo
 
     public override double CalcularConsumo(double kilometros)
     {
-        double extras =
-            DateTime.Now.Year - GetAnio() > 5
-            ? (kilometros / 15) * litrosExtra
-            : 0;
-
-        double total = (kilometros / kilometrosPorLitro) + extras;
-
-        return total;
+        return kilometros * kilometrosPorLitro;
     }
 }
